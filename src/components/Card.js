3f034a611f4b4f4dd3bd2e6cd515c553b4230295
Andrew <andrewk745@gmail.com>
2022-02-05 15:48:08 -0500

    added loading skeleton for card image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardHeader, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
-import Skeleton from 'react-loading-skeleton'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function Card({src, title, onLoad, loaded}) {
   return (
     <MDBCard>
-      <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+      <Skeleton width={400} height={550} style={loaded ? {display: 'none'} : {}}/>
+      <MDBRipple style={loaded ? {} : {display: 'none'}} rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
         <MDBCardImage onLoad={onLoad} src={src} fluid alt='...' />
         <a>
           <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
