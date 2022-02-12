f4ffee696ed77b3215b710174adf9a0af7634f2a
Andrew Kim <andrewk745@gmail.com>
2022-02-12 06:40:31 -0500

    fixed up shop reducer logic

--- src/redux/actions/shopping-actions.js
+++ src/redux/actions/shopping-actions.js
@@ -4,15 +4,15 @@ import * as actionTypes from '../constants/shopping-types'
 export const addToCart = (gameID, qty) => (dispatch, getState) => {
     console.log('getstate in addToCart', getState().getTopGames.games)
     const item = getState().getTopGames.games.find((game) => game.id === gameID);
-    console.log(item)
+    console.log('item from getState', item)
     dispatch ({
       type: actionTypes.ADD_TO_CART,
       payload: {
-        id: gameID,
+        item: item,
         qty,
       },
     }); 
-    localStorage.setItem("cart", JSON.stringify(getState().shop.cart));
+    localStorage.setItem("cart", JSON.stringify(item));
   };
 
 export const removeFromCart = (gameID) => {
--- src/redux/reducers/shopping-reducer.js
+++ src/redux/reducers/shopping-reducer.js
@@ -3,28 +3,28 @@ import * as actionTypes from '../constants/shopping-types'
 
 
 const INITIAL_STATE = {
-    cartItems: [],
+    cart: [],
 
 }
 
 export const shopReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case actionTypes.ADD_TO_CART:
-            const item = state.games.find(
-                (game) => game.id === action.payload.id
-              );
+            console.log('action payload',action.payload)
+            const item = action.payload.item
               console.log('item', item)
-              console.log(state.cart)
+              console.log('in shopreducer', state.cart)
               // Check if Item is in cart already
               const inCart = state.cart.find((item) =>
-                item.id === action.payload.id ? true : false
+                item.id === action.payload.item.id ? true : false
               );
+              console.log('inCart', inCart)
         
               return {
                 ...state,
                 cart: inCart
                   ? state.cart.map((item) =>
-                      item.id === action.payload.id
+                      item.id === action.payload.item.id
                         ? { ...item, qty: item.qty + 1 }
                         : item
                     )
